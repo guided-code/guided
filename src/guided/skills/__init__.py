298c023725a51c0d0137dfd00549190391845b7d
Henry Tseng <henry@heycanvas.com>
2026-03-17 10:50:38 -0400

    Initial resource
    Signed-off-by: Henry Tseng <henry@heycanvas.com>

--- src/guided/skills/__init__.py
+++ src/guided/skills/__init__.py
@@ -1,5 +1,14 @@
 from guided.skills.web_search import search_web_text, request_web
 from guided.skills.container import exec_command, list_files, read_file, write_file
+from guided.skills.kubernetes import (
+    kubectl_get,
+    kubectl_describe,
+    kubectl_logs,
+    kubectl_exec,
+    kubectl_apply,
+    kubectl_delete,
+    kubectl_rollout_status,
+)
 
 DEFAULT_TOOLS = [
     search_web_text,
@@ -8,4 +17,11 @@ DEFAULT_TOOLS = [
     list_files,
     read_file,
     write_file,
+    kubectl_get,
+    kubectl_describe,
+    kubectl_logs,
+    kubectl_exec,
+    kubectl_apply,
+    kubectl_delete,
+    kubectl_rollout_status,
 ]
