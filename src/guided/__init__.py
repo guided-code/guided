0650d19a298e466ec7c8e9f8b5b2251b26eeef4a
Henry Tseng <henry@heycanvas.com>
2026-03-26 16:48:38 -0400

    Fixes version query
    Signed-off-by: Henry Tseng <henry@heycanvas.com>

--- src/guided/__init__.py
+++ src/guided/__init__.py
@@ -1,15 +1,19 @@
 from dunamai import Version
+from importlib.metadata import version
 
 
 def get_version() -> str:
     """
     Autodetect VCS and get the version
     """
-    version = Version.from_any_vcs()
-    return version.serialize(
-        metadata=True,
-        dirty=True,
-    )
+    try:
+        semver = Version.from_any_vcs()
+        return semver.serialize(
+            metadata=True,
+            dirty=True,
+        )
+    except RuntimeError:
+        return version("guided")
 
 
 __version__ = get_version()
